5a6949162252b2c1acdba9681d90948bbdffdc4c
Anton Kutishevsky <akutishevsky@gmail.com>
2024-07-03 11:32:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# LWC Searchable Combobox
+
+## Demo
+
+![Alt text](demo.gif)
+
+This repository contains the source code for the answer to [this Salesforce Stack Exchange Question.](https://salesforce.stackexchange.com/questions/395758/searchable-lightning-combobox-lwc/395787#395787)
---
+++ force-app/main/default/lwc/searchableCombobox/searchableCombobox.js
@@ -0,0 +1,67 @@
+import { LightningElement } from "lwc";
+import getAccounts from "@salesforce/apex/SearchableComboboxController.getAccounts";
+
+export default class SearchableCombobox extends LightningElement {
+    pickListOrdered;
+    searchResults;
+    selectedSearchResult;
+
+    get selectedValue() {
+        return this.selectedSearchResult
+            ? this.selectedSearchResult.label
+            : null;
+    }
+
+    connectedCallback() {
+        getAccounts().then((result) => {
+            this.pickListOrdered = result.sort((a, b) =>
+                a.label.localeCompare(b.label)
+            );
+        });
+    }
+
+    isListening = false;
+    renderedCallback() {
+        if (this.isListening) return;
+
+        window.addEventListener("click", (event) => {
+            this.hideDropdown(event);
+        });
+        this.isListening = true;
+    }
+
+    hideDropdown(event) {
+        const cmpName = this.template.host.tagName; // C-SEARCHABLE-COMBOBOX
+        const clickedElementSrcName = event.srcElement.tagName; // whatever was clicked on the page
+        const isClickedOutside = cmpName !== clickedElementSrcName;
+        if (this.searchResults && isClickedOutside) {
+            this.clearSearchResults();
+        }
+    }
+
+    search(event) {
+        const input = event.detail.value.toLowerCase();
+        const result = this.pickListOrdered.filter((pickListOption) =>
+            pickListOption.label.toLowerCase().includes(input)
+        );
+        this.searchResults = result;
+    }
+
+    selectSearchResult(event) {
+        const selectedValue = event.currentTarget.dataset.value;
+        this.selectedSearchResult = this.pickListOrdered.find(
+            (pickListOption) => pickListOption.value === selectedValue
+        );
+        this.clearSearchResults();
+    }
+
+    clearSearchResults() {
+        this.searchResults = null;
+    }
+
+    showPickListOptions() {
+        if (!this.searchResults) {
+            this.searchResults = this.pickListOrdered;
+        }
+    }
+}
